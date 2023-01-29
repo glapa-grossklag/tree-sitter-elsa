b0cfef22390399bd4654e84db46d33bad83146c8
Amaan Qureshi <amaanq12@gmail.com>
2023-01-28 20:26:26 -0500

    feat: distinguish between functions, methods, parameters

--- grammar.js
+++ grammar.js
@@ -1,19 +1,55 @@
-module.exports = grammar({
-    name : 'elsa',
+/* eslint-disable camelcase */
+/* eslint-disable-next-line spaced-comment */
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
 
-    rules : {
-        source_file : $ => seq(repeat($.definition), repeat($.reduction)),
-        definition : $ => seq('let', $.identifier, '=', $.term),
-        reduction : $ =>
-            seq('eval', $.identifier, ':', $.term, repeat(seq($.step, $.term))),
-        identifier : $ => /[A-Za-z_]+[A-Za-z_0-9]*/,
-        abstraction : $ => seq('\\', field('parameters', repeat1($.identifier)),
-                               '->', field('body', $.term)),
-        application : $ => prec.left(
-            1, seq(field('abstraction', $.term), field('argument', $.term))),
-        term : $ => choice(seq('(', $.term, ')'),
-                           choice($.identifier, $.abstraction, $.application)),
-        step : $ => choice('=a>', '=b>', '=d>', '=*>', '=~>'),
-        comment : $ => /--.*/
-    }
+module.exports = grammar({
+  name: 'elsa',
+
+  extras: ($) => [/\s/, $.comment],
+
+  rules: {
+    source_file: ($) => seq(repeat($.definition), repeat($.reduction)),
+
+    definition: ($) => seq(
+        'let',
+        alias($.identifier, $.function),
+        '=',
+        $.term,
+    ),
+
+    reduction: ($) =>
+      seq(
+          'eval',
+          alias($.identifier, $.method),
+          ':',
+          $.term,
+          repeat(seq($.step, $.term)),
+      ),
+
+    abstraction: ($) => seq(
+        '\\',
+        field('parameters', repeat1(alias($.identifier, $.parameter))),
+        '->',
+        field('body', $.term),
+    ),
+
+    application: ($) => prec.left(1,
+        seq(
+            field('abstraction', $.term),
+            field('argument', $.term),
+        ),
+    ),
+
+    term: ($) => choice(
+        seq('(', $.term, ')'),
+        choice($.identifier, $.abstraction, $.application),
+    ),
+
+    step: () => choice('=a>', '=b>', '=d>', '=*>', '=~>'),
+
+    identifier: () => /[A-Za-z_]+[A-Za-z_0-9]*/,
+
+    comment: () => seq('--', /(\\(.|\r?\n)|[^\\\n])*/),
+  },
 });
